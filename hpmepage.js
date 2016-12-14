270c69feddaa87fac4507b164483705624cf44ed
stonewallstan <stanleyarmstrong31@gmail.com>
2016-12-13 21:51:45 -0800

    Lets see what this does

--- hpmepage.js
+++ hpmepage.js
@@ -1,4 +1,4 @@
 var element = document.getElementById('welcomToSlack');
 element.onclick = function redirect(){
-  location.href = "https://slack.hackclub.com/";
+  window.top,location.href = "https://slack.hackclub.com/";
 };
